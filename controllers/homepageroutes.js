9b7575243793b26a164e4a8e7c71cee23b4d9358
Ownerman123 <charlesshumway1@gmail.com>
2024-06-11 11:11:23 -0600

    fixed logout on login page for wierdos who visit the page while already logged in somehow

--- controllers/homepageroutes.js
+++ controllers/homepageroutes.js
@@ -19,6 +19,7 @@ router.get("/login", async (req, res) => {
     try {
       res.render("login", {
         // objects for info to dynamically put to the page
+        logged_in: req.session.logged_in
       });
     } catch (err) {
       res.status(500).json(err);
