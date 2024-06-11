86fcee9143d09362deca6135ce7c4aa636c63b63
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-06-11 14:00:18 -0600

    rebuilt the .gitignore file, causing many issues

--- controllers/homepageroutes.js
+++ controllers/homepageroutes.js
@@ -1,19 +1,32 @@
 const express = require("express");
-
 const router = express.Router();
+const { Listing, User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("homepage", {
-      // objects for info to dynamically put to the page
-    //   logged_in: req.session.logged_in
-      logged_in: false
+    const listingData = await Listing.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'], // Only include the user's name
+        },
+      ],
+      order: [["createdAt", "DESC"]] // Assuming you want the newest posts first
     });
+
+    const listings = listingData.map((listing) => listing.get({ plain: true }));
+    console.log(listings);
+    res.render("homepage", {
+      listings,
+      logged_in: true
+    });
+
   } catch (err) {
+    console.error("Failed to fetch listings:", err);
     res.status(500).json(err);
-    console.log("dang");
   }
 });
+
 
 router.get("/login", async (req, res) => {
     try {
